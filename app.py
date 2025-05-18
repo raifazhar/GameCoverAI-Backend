0b29547f35d476126823f4ecdf2f3ddb53dccd6f
NewUsername <newemail@example.com>
2025-05-18 18:20:36 +0500

    fix

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, jsonify
 import tensorflow as tf
 from PIL import Image
@@ -30,4 +31,5 @@ def predict():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))  # Use Railway PORT or default 5000 locally
+    app.run(host="0.0.0.0", port=port)
